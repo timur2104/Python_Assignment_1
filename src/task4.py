51dea9629e096230cc7cf67823de31652bd05f5c
Вячеслав Блинов <gravitycat35@gmail.com>
2022-09-20 18:09:02 +0300

    Code Review

--- src/task4.py
+++ src/task4.py
@@ -1,7 +1,6 @@
+import contextlib
 import datetime
 import time
-import contextlib
-import io
 import traceback
 
 
@@ -13,16 +12,22 @@ class ProtectedClassDecorator1:
     def __call__(self, *args, **kwargs):
         self.count += 1
 
-        start = time.perf_counter()
-        with open("output.txt", "a") as output_file, open('exceptions.log', 'a') as log:
-            with contextlib.redirect_stdout(output_file), contextlib.redirect_stderr(log):
+        with open("output.txt", "a") as output_file, open(
+            "exceptions.log", "a"
+        ) as log_file:
+            with contextlib.redirect_stdout(output_file):
                 try:
+                    start = time.perf_counter()
                     output = self.func(*args, **kwargs)
                 except:
-                    log.write(f'Dropped exception at {datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}:\n"{traceback.format_exc().strip()}"\n')
+                    log_file.write(
+                        f'Dropped exception at {datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}:\n"{traceback.format_exc().strip()}"\n'
+                    )
                 else:
                     end = time.perf_counter()
-                    print(f"{self.func.__name__} call {self.count} executed in {end - start} sec")
+                    print(
+                        f"{self.func.__name__} call {self.count} executed in {end - start} sec."
+                    )
 
                     return output
 
@@ -30,16 +35,22 @@ class ProtectedClassDecorator1:
 def protected_decorator_1(func):
     def wrapper(*args, **kwargs):
         wrapper.count += 1
-        start = time.perf_counter()
-        with open("output.txt", "a") as output_file, open('exceptions.log', 'a') as log:
-            with contextlib.redirect_stdout(output_file), contextlib.redirect_stderr(log):
+
+        with open("output.txt", "a") as output_file, open("exceptions.log", "a") as log:
+            with contextlib.redirect_stdout(output_file):
                 try:
-                    output = func(*args, **kwargs)
+                    start = time.perf_counter()
+                    func(*args, **kwargs)
                 except:
-                    log.write(f'Dropped exception at {datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}:\n"{traceback.format_exc().strip()}"\n')
+                    log.write(
+                        f'Dropped exception at {datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}:\n"{traceback.format_exc().strip()}"\n'
+                    )
                 else:
                     end = time.perf_counter()
-                    print(f"{func.__name__} call {wrapper.count} executed in {end - start} sec")
+                    print(
+                        f"{func.__name__} call {wrapper.count} executed in {end - start} sec."
+                    )
 
     wrapper.count = 0
+
     return wrapper
